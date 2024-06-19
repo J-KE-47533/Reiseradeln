d33799ec32d40fabe24da97416cc58530a9bc9f7
J-KE-47533 <82226827+J-KE-47533@users.noreply.github.com>
2024-06-19 07:01:08 +0000

    Content Preview js hinzugefügt

---
+++ js/prev.js
@@ -0,0 +1,23 @@
+function loadContent(page, elementId) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            // Erstellen eines temporären DOM-Elements
+            let tempDiv = document.createElement('div');
+            tempDiv.innerHTML = data;
+
+            // Finden des ersten p-Tags und Extrahieren der ersten 15 Wörter
+            let paragraph = tempDiv.querySelector('p').innerText;
+            let words = paragraph.split(' ').slice(0, 10).join(' ') + " [...]";
+
+            // Anzeige auf der index.html
+            document.getElementById(elementId).innerText = words;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+loadContent('beine.html', 'beine-content');
+loadContent('brust.html', 'brust-content');
+// loadContent('bauch.html', 'bauch-content');
+// loadContent('ruecken.html', 'rücken-content');
